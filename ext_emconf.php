cb149711259a9581e48b6609830c911538ebbd05
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2023-11-07 20:49:03 +0100

    Updated dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.11-11.5.99'
+            'typo3' => '10.4.11-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => []
